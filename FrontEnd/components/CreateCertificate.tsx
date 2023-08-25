2223138538d4cb42c6e372f37ad735b0e8d0cbfa
WiseMrMusa <abdulkareemusa01@gmail.com>
2023-08-25 07:49:48 +0100

    added ui components

---
+++ FrontEnd/components/CreateCertificate.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image';
+import { ButtonLink } from '../components/Button';
+import { Container } from '../components/Container';
+
+import React from 'react';
+
+export function CreateCertificate() {
+    return (
+        <Container className="pt-20 pb-16 text-center lg:pt-32">
+
+        </Container>
+    );
+}
